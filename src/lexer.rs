ffbf4f4ff5ef3424f31ad5c990fb88984b1723b1
ThoNohT <e.c.p.bataille@gmail.com>
2021-12-14 17:28:55 +0100

    Implement lexer

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,4 +1,5 @@
 /// A position for a token.
+#[derive(Copy, Clone)]
 pub struct Position {
     line: u64,
     col: u64,
@@ -22,15 +23,16 @@ impl Position {
     }
 }
 
-pub struct Range<'a> {
-    file: &'a str,
+#[derive(Clone)]
+pub struct Range {
+    file: String,
     start_line: u64,
     start_col: u64,
     end_line: u64,
     end_col: u64,
 }
 
-impl<'a> Range<'a> {
+impl Range {
     /// Converts a range to a string that points to a the starting position of the range in its file.
     pub fn to_string(self: &Self) -> String {
         format!(
@@ -54,7 +56,7 @@ impl<'a> Range<'a> {
     }
 
     /// Creates a range from a file and a start and end position.
-    pub fn from_positions(file: &str, start_pos: Position, end_pos: Position) -> Range {
+    pub fn from_positions(file: String, start_pos: Position, end_pos: Position) -> Range {
         Range {
             file,
             start_line: start_pos.line,
@@ -82,20 +84,20 @@ impl<'a> Range<'a> {
 }
 
 /// Encodes all the different types of tokens, with their data.
-#[derive(Debug)]
-pub enum TokenData<'a> {
+#[derive(Clone, Debug)]
+pub enum TokenData {
     IndentationToken(u64),
-    KeywordToken(&'a str),
-    IdentifierToken(&'a str),
-    SymbolToken(&'a str),
-    StringLiteralToken(&'a str),
+    KeywordToken(String),
+    IdentifierToken(String),
+    SymbolToken(String),
+    StringLiteralToken(String),
     NumberToken(i64),
     SeparatorToken(),
     EndOfLineToken(),
     EndOfInputToken(),
 }
 
-impl<'a> TokenData<'a> {
+impl TokenData {
     /// Returns a string that can be used for displaying a token in error messages.
     /// withValue determines whether the value of the token is also included.
     /// Note that for string literals, the value is never included.
@@ -145,7 +147,7 @@ impl<'a> TokenData<'a> {
 
     /// Returns the value of an identifier in a token, if it is an identifier token, None
     /// otherwise.
-    pub fn try_get_identifier(self: Self) -> Option<&'a str> {
+    pub fn try_get_identifier(self: Self) -> Option<String> {
         match self {
             Self::IdentifierToken(i) => Some(i),
             _ => None,
@@ -154,7 +156,7 @@ impl<'a> TokenData<'a> {
 
     /// Returns the value of a string literal in a token, if it is a string literal token, None
     /// otherwise.
-    pub fn try_get_string_literal(self: Self) -> Option<&'a str> {
+    pub fn try_get_string_literal(self: Self) -> Option<String> {
         match self {
             Self::StringLiteralToken(sl) => Some(sl),
             _ => None,
@@ -163,30 +165,426 @@ impl<'a> TokenData<'a> {
 
     /// Returns the value of a number in a token, if it is a number token, Noen otherwise.
     pub fn try_get_number(self: Self) -> Option<i64> {
-         match self {
-             Self::NumberToken(n) => Some(n),
-             _ => None,
-         }
+        match self {
+            Self::NumberToken(n) => Some(n),
+            _ => None,
+        }
     }
 }
 
 /// A token produced by the lexer, adds information about the token's location and whether it was
-/// preceded by  whitespace.
-pub struct Token<'a> {
-    range: Range<'a>,
-    data: TokenData<'a>,
+/// preceded by whitespace.
+#[derive(Clone)]
+pub struct Token {
+    range: Range,
+    data: TokenData,
     whitespace_before: bool,
 }
 
-impl<'a> Token<'a> {
+impl Token {
     /// Converts a token to a string that can be used for debugging purposes.
     pub fn to_string(self: Self) -> String {
-        format!("[ {} ; whitespaceBefore: {} ; {:?} ]", self.range.to_full_string(), self.whitespace_before, self.data)
+        format!(
+            "[ {} ; whitespaceBefore: {} ; {:?} ]",
+            self.range.to_full_string(),
+            self.whitespace_before,
+            self.data
+        )
     }
 }
 
-use std::collections::HashSet;
+pub mod lexer {
+    use crate::{
+        console::ReturnOnError,
+        lexer::{Position, Range, Token, TokenData},
+    };
+    use core::str::Chars;
+    use std::{char, collections::HashMap, collections::HashSet};
 
-pub fn lex_file<'a>(keywords: HashSet<&str>, file_name: &str, input: &str) -> Vec<Token<'a>> {
-    unimplemented!()
+    /// An iterator over an input string that skips characters that should be ignored
+    /// by the lexer.
+    #[derive(Clone)]
+    struct LexerInput<'a>(Chars<'a>);
+
+    impl<'a> Iterator for LexerInput<'a> {
+        type Item = char;
+
+        fn next(&mut self) -> Option<char> {
+            loop {
+                let next = self.0.next()?;
+                if next != '\r' {
+                    return Some(next);
+                }
+            }
+        }
+    }
+
+    struct LexerState<'a> {
+        filename: &'a str,
+
+        // Primary input.
+        input: LexerInput<'a>,
+        index: u64,
+        pos: Position,
+        prev_pos: Position,
+
+        // Backup input.
+        backup_input: LexerInput<'a>,
+        backup_index: u64,
+        backup_pos: Position,
+
+        // Output.
+        tokens: Vec<Token>,
+
+        // Other state.
+        cur_char: char,
+        start_of_line: bool,
+        spaces_per_indent: Option<u64>,
+        whitespace_before: bool,
+        at_end_of_input: bool,
+    }
+
+    impl<'a> LexerState<'a> {
+        fn new(input: &'a str, filename: &'a str) -> Self {
+            let mut sanitized_input = LexerInput(input.chars());
+            Self {
+                filename,
+
+                // Primary input.
+                index: 0,
+                pos: Position::zero(),
+                prev_pos: Position::zero(),
+
+                // Backup input.
+                backup_input: sanitized_input.clone(),
+                backup_index: 0,
+                backup_pos: Position::zero(),
+
+                // Output.
+                tokens: Vec::new(),
+
+                // Other state.
+                cur_char: sanitized_input.next().unwrap_or('\0'),
+                start_of_line: true,
+                spaces_per_indent: None,
+                whitespace_before: false,
+                at_end_of_input: false,
+
+                input: sanitized_input,
+            }
+        }
+
+        /// Advance the index, and the position of the lexer.
+        fn next_char(self: &mut Self) {
+            if self.at_end_of_input {
+                return;
+            }
+
+            // Determine the next position.
+            self.prev_pos = self.pos.clone();
+            match self.cur_char {
+                '\n' => {
+                    self.pos.line = self.pos.line + 1;
+                    self.pos.col = 0;
+                }
+                _ => {
+                    self.pos.col = self.pos.col + 1;
+                }
+            }
+
+            // Determine the next character.
+            let next_char = self.input.next();
+            match next_char {
+                Some(c) => {
+                    self.cur_char = c;
+                    self.index = self.index + 1;
+                }
+                None => {
+                    self.at_end_of_input = true;
+                    self.cur_char = '\0';
+                }
+            }
+        }
+
+        /// Sets the backup variables to the current position of the lexer, so it can be restored
+        /// to this state later if needed.
+        fn set_backup_point(self: &mut Self) {
+            self.backup_pos = self.pos.clone();
+            self.backup_index = self.backup_index;
+            self.backup_input = self.input.clone();
+        }
+
+        /// Adds a token to the output list.
+        /// Uses the state's backup position as the start of the token, and the previous position
+        /// as the end.
+        fn add_token(self: &mut Self, data: TokenData) {
+            let tkn = Token {
+                range: Range::from_positions(
+                    self.filename.to_string(),
+                    self.backup_pos.clone(),
+                    self.prev_pos.clone(),
+                ),
+                whitespace_before: self.whitespace_before,
+                data: data.clone(),
+            };
+            self.whitespace_before = false;
+            self.tokens.push(tkn);
+        }
+
+        /// Can be used to check a predicate, and if it fials, exit with the specified error
+        /// message, including some location data.
+        fn check_lexer_predicate(self: &Self, pred: bool, msg: &str) {
+            let msg = format!(
+                "{}: Lexer error: {}",
+                self.pos.to_file_string(self.filename),
+                msg
+            );
+            crate::console::test_condition(pred, msg.as_str());
+        }
+
+        fn assert_not_at_end(self: &Self, msg: &str) {
+            self.check_lexer_predicate(!self.at_end_of_input, msg);
+        }
+
+        fn assert_digit(self: &Self, msg: &str) {
+            self.check_lexer_predicate(self.cur_char.is_digit(10), msg);
+        }
+    }
+
+    /// Custom version of char::is_whitespace that excludes the newline character.
+    fn is_whitespace(c: char) -> bool {
+        c.is_whitespace() && c != '\n'
+    }
+
+    /// Lexes an indentation token, consisting of spaces at the sart of a line.
+    /// Only allows spaces as indentation. The lexe will fail when it encounters any other
+    /// whitespace character, or when an unexepected number of spaces is encountered.
+    fn lex_indent(state: &mut LexerState) {
+        state.set_backup_point();
+
+        match state.spaces_per_indent {
+            None => {
+                // The number of spaces per indent is not yet known, the total number of spaces
+                // encountered during the f irst time leading whitespace occurs is taken as the
+                // number of spaces per indent.
+                while !state.at_end_of_input && is_whitespace(state.cur_char) {
+                    state.check_lexer_predicate(
+                        state.cur_char == ' ',
+                        "Leading whitespace may only consist of spaces.",
+                    );
+                    state.next_char();
+                }
+
+                state.spaces_per_indent = Some(state.index - state.backup_index);
+                state.add_token(TokenData::IndentationToken(1));
+            }
+            Some(spi) => {
+                // The number of spaces per indent is known, so take a multiple of this number of
+                // spaces, and re turn this as the indent level.
+                while !state.at_end_of_input && is_whitespace(state.cur_char) {
+                    state.check_lexer_predicate(
+                        state.cur_char == ' ',
+                        "Leading whitespace may only consist of spaces.",
+                    );
+                    state.next_char();
+                }
+
+                let n_spaces = state.index - state.backup_index;
+                let extra_space = n_spaces % spi;
+                let msg = format!(
+                    "{} {}, but got {}, which is {} too many or {} too few.",
+                    "Invalid number of leading spaces. Expected a multiple of",
+                    spi,
+                    n_spaces,
+                    extra_space,
+                    (spi - extra_space)
+                );
+                state.check_lexer_predicate(extra_space == 0, msg.as_str());
+                state.add_token(TokenData::IndentationToken(n_spaces / spi));
+            }
+        }
+    }
+
+    /// Lexes a number, simply a token with a value as long as the characters are numeric.
+    fn lex_number(state: &mut LexerState) {
+        state.set_backup_point();
+        let mut nr_str = String::with_capacity(20);
+        while !state.at_end_of_input && state.cur_char.is_digit(10) {
+            nr_str.push(state.cur_char);
+            state.next_char();
+        }
+        let nr = nr_str
+            .parse::<i64>()
+            .map_err(|e| format!("{}", e))
+            .handle_with_exit(Some("Failed to parse a number."));
+        state.add_token(TokenData::NumberToken(nr));
+    }
+
+    /// Lexes an identifier, consumes characters as long as they are alphanumeric. If the resulting
+    /// name is cont aqined in the set of keywords, a keyword token is returned, otherwise an
+    /// identifier token is returned.
+    fn lex_identifier(state: &mut LexerState, keywords: &HashSet<&str>) {
+        state.set_backup_point();
+        let mut id_str = String::with_capacity(8);
+        while !state.at_end_of_input && state.cur_char.is_alphanumeric() {
+            id_str.push(state.cur_char);
+            state.next_char();
+        }
+
+        if keywords.contains(id_str.as_str()) {
+            state.add_token(TokenData::KeywordToken(id_str));
+        } else {
+            state.add_token(TokenData::IdentifierToken(id_str));
+        }
+    }
+
+    /// Lexes a string literal.
+    fn lex_string_literal(state: &mut LexerState) {
+        fn assert_not_at_end(state: &LexerState) {
+            state.check_lexer_predicate(
+                !state.at_end_of_input,
+                "Input ended before escaped character in string literal ended.",
+            );
+        }
+
+        fn assert_digit(state: &LexerState) {
+            state.check_lexer_predicate(!state.cur_char.is_digit(10), "Invalid escaped character.");
+        }
+
+        // An escaped character can be a charcter from mapped_chars.
+        // If the character was not mapped, return the escape character and the character itself.
+        fn lex_escaped_char(state: &mut LexerState) -> String {
+            let mapped_chars = HashMap::from([
+                ('\\', '\\'),
+                ('/', '/'),
+                ('b', '\x08'),
+                ('f', '\x0c'),
+                ('n', '\n'),
+                ('r', '\r'),
+                ('t', '\t'),
+            ]);
+
+            state.next_char();
+            assert_not_at_end(state);
+            match mapped_chars.get(&state.cur_char) {
+                Some(mc) => format!("{}", mc),
+                None => format!("\\{}", state.cur_char),
+            }
+        }
+
+        state.set_backup_point();
+
+        // Go to the first character that makes up the string.
+        state.next_char();
+
+        // Parse string characters until we reached the closing quote.
+        let mut str = String::with_capacity(32);
+        while !state.at_end_of_input && state.cur_char != '"' {
+            if state.cur_char == '\\' {
+                str.push_str(lex_escaped_char(state).as_str());
+            } else {
+                str.push(state.cur_char);
+            }
+
+            // Move to the next character.
+            state.next_char();
+        }
+
+        // Consume the closing quote.
+        assert_not_at_end(state);
+        state.next_char();
+
+        state.add_token(TokenData::StringLiteralToken(str));
+    }
+
+    fn lex_symbol(state: &mut LexerState) {
+        fn is_symbol_char(c: char) -> bool {
+            !c.is_whitespace() && !c.is_alphanumeric()
+        }
+
+        fn lex_regular_symbol(state: &mut LexerState, first_char: char) {
+            let mut sym_str = String::from(first_char);
+
+            while !state.at_end_of_input && is_symbol_char(state.cur_char) {
+                sym_str.push(state.cur_char);
+                state.next_char();
+            }
+
+            state.add_token(TokenData::SymbolToken(sym_str));
+        }
+
+        state.set_backup_point();
+        let first_char = state.cur_char;
+
+        state.next_char();
+
+        match state.cur_char {
+            '-' => {
+                if !state.at_end_of_input && state.cur_char == '-' {
+                    // More than one - indicates we are at a separator character.
+                    while !state.at_end_of_input && state.cur_char == '-' {
+                        state.next_char();
+                    }
+                    state.add_token(TokenData::SeparatorToken());
+                } else {
+                    lex_regular_symbol(state, first_char);
+                }
+            }
+            '/' => {
+                if !state.at_end_of_input && state.cur_char == '/' {
+                    // Two / indicate a single-line comment. Just move on until a newline is
+                    // consumed and throw everything away.
+                    while !state.at_end_of_input && state.cur_char != '\n' {
+                        state.next_char();
+                    }
+
+                    // Move on to the next line.
+                    if !state.at_end_of_input {
+                        state.next_char();
+                    }
+                } else {
+                    lex_regular_symbol(state, first_char);
+                }
+            }
+            _ => {
+                lex_regular_symbol(state, first_char);
+            }
+        }
+    }
+
+    pub fn lex_file<'a, 'b>(
+        keywords: HashSet<&str>,
+        filename: &'a str,
+        input: &'a str,
+    ) -> Vec<Token> {
+        // TODO: Grapheme clusters?
+        let mut state = LexerState::new(input, filename);
+
+        while !state.at_end_of_input {
+            if is_whitespace(state.cur_char) && state.start_of_line {
+                lex_indent(&mut state);
+            } else if is_whitespace(state.cur_char) {
+                state.whitespace_before = true;
+                while !state.at_end_of_input && is_whitespace(state.cur_char) {
+                    state.next_char();
+                }
+            } else if state.cur_char.is_digit(10) {
+                lex_number(&mut state);
+            } else if state.cur_char.is_alphabetic() {
+                lex_identifier(&mut state, &keywords);
+            } else if state.cur_char == '"' {
+                lex_string_literal(&mut state);
+            } else if state.cur_char == '\n' {
+                state.add_token(TokenData::EndOfLineToken());
+                state.next_char();
+            } else {
+                lex_symbol(&mut state);
+            }
+        }
+
+        state.add_token(TokenData::EndOfInputToken());
+
+        // Return the built list of tokens.
+        state.tokens
+    }
 }
